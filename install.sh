adccfa6b9675b4aa746da12171534665ed104737
Nick Strand <nick@strand.family>
2024-03-30 12:02:54 -0500

    compose update

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-# apt-get update -y && apt-get upgrade -y
-# apt-get install python3 python3-pip -y
-# pip3 install requests
-# python3 /redirect.py
-pip install requests
+pip install requests environs
 python /redirect.py
